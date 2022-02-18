8f1d8940c62cd524b2ef4bf7ffe91c29b657b182
giantrix8 <96435444+giantrix8@users.noreply.github.com>
2022-02-18 16:34:27 +0100

    Add files via upload

--- DAO/NumeroTelefonoFissoDAO.java
+++ DAO/NumeroTelefonoFissoDAO.java
@@ -7,8 +7,22 @@ import java.util.ArrayList;
 import Model.DBConnection;
 import Model.NumeroTelefonoFisso;
 
+
+/**
+ * Classe di collegamento tra il numero telefonico e il database.
+ */
 public class NumeroTelefonoFissoDAO implements DAO<NumeroTelefonoFisso> {
+	
+	/** dbc è la variabile di connessione con il database. */
 	private DBConnection dbc = DBConnection.getInstance();
+	
+	/**
+	 * classe lanciatrice di eccezioni sql
+	 *
+	 * @param sql: stringa 
+	 * @return numero telefono fisso
+	 * @throws SQLException 
+	 */
 	@Override
 	public NumeroTelefonoFisso get(String sql) throws SQLException {
 		ResultSet rs = dbc.executeQuery(sql);
@@ -22,6 +36,13 @@ public class NumeroTelefonoFissoDAO implements DAO<NumeroTelefonoFisso> {
 		}
 	}
 
+	/**
+	 * Gets the all.
+	 *
+	 * @param sql the sql
+	 * @return the all
+	 * @throws SQLException the SQL exception
+	 */
 	@Override
 	public ArrayList<NumeroTelefonoFisso> getAll(String sql) throws SQLException {
 		ResultSet rs = dbc.executeQuery(sql);
@@ -32,24 +53,50 @@ public class NumeroTelefonoFissoDAO implements DAO<NumeroTelefonoFisso> {
 		}
 		return list;
 	}
+	
+	/**
+	 * Extract.
+	 *
+	 * @param rs the rs
+	 * @return the numero telefono fisso
+	 * @throws SQLException the SQL exception
+	 */
 	private NumeroTelefonoFisso extract(ResultSet rs) throws SQLException
 	{
 		return new NumeroTelefonoFisso(rs.getString("numero"));
 	}
 
 
+	/**
+	 * Update.
+	 *
+	 * @param obj the obj
+	 * @throws SQLException the SQL exception
+	 */
 	@Override
 	public void update(NumeroTelefonoFisso obj) throws SQLException {
 		String sql = "UPDATE NumeroTelefono SET numero = " + obj.getNumero() + " WHERE numero = " + obj.getNumero();
 		dbc.executeQuery(sql);
 	}
 
+	/**
+	 * Delete.
+	 *
+	 * @param obj the obj
+	 * @throws SQLException the SQL exception
+	 */
 	@Override
 	public void delete(NumeroTelefonoFisso obj) throws SQLException {
 		String sql = "DELETE FROM NumeroTelefono WHERE numero = " + obj.getNumero();
 		dbc.execute(sql);
 	}
 
+	/**
+	 * Insert.
+	 *
+	 * @param obj the obj
+	 * @throws SQLException the SQL exception
+	 */
 	@Override
 	public void insert(NumeroTelefonoFisso obj) throws SQLException {
 		String sql = "INSERT INTO NumeroTelefono(numero) VALUES(" + obj.getNumero() + ")";
